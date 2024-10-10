00de7c926eefd93696d5bb07ebcb298ad6d0f2c3
Naga Mohan Reddy Karri <129124976+NagaMohanReddyKarri@users.noreply.github.com>
2024-10-10 00:26:17 -0500

    updated

---
+++ src/components/CustomersSay/index.tsx
@@ -0,0 +1,33 @@
+import { FC, HTMLAttributes } from "react";
+import { Container, Title, Cards } from "./styles";
+import CustomersSayCard from "../CustomersSayCard";
+
+const CustomersSay: FC<HTMLAttributes<HTMLDivElement>> = (props): JSX.Element => {
+  return (
+    <Container {...props}>
+      <Title>What ours customers say!</Title>
+      <Cards>
+        <CustomersSayCard
+          rating={5}
+          customerImg="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+          customerName="Mohan"
+          testimonial="I loved the ambience! The food was delicious and the service was excellent. I will definitely be back!"
+        />
+        <CustomersSayCard
+          rating={4}
+          customerImg="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+          customerName="Pavani"
+          testimonial="I went to the restaurant with my family and enjoyed a lot. The food was wonderful and the atmosphere was very cozy. I highly recommend it!"
+        />
+        <CustomersSayCard
+          rating={3}
+          customerImg="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+          customerName="Mike"
+          testimonial="I have been to many restaurants, but this one certainly stood out. The quality of the ingredients and the preparation of the dishes were impeccable. Not to mention the presentation, which was beautiful. It was an amazing dining experience!"
+        />
+      </Cards>
+    </Container>
+  );
+};
+
+export default CustomersSay;
